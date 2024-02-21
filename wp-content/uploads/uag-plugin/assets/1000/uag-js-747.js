fd75aa6a03d79ce358ed66b9e53900b4520d71f7
Arifin <arifin@Nurs-MacBook-Air.local>
2024-02-21 18:17:11 +0100

    event

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-747.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCountdown.init( '.uagb-block-72b2894a', {"block_id":"72b2894a","endDateTime":"2024-04-07T18:30:00.000Z","showDays":true,"showHours":true,"showMinutes":true,"isFrontend":true,"timerEndAction":"zero","redirectURL":""} );
+});
+window.addEventListener("DOMContentLoaded", function(){
+	UAGBTimelineClasses( {"block_id":"30a89927","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-30a89927' );
+});
+window.addEventListener("resize", function(){
+	UAGBTimelineClasses( {"block_id":"30a89927","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-30a89927' );
+});
+ });
