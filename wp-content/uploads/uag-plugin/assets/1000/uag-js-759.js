6cebb245c7fa239f81d24d38b3081e021498eb71
Arifin <arifin@Nurs-MacBook-Air.local>
2024-03-06 12:12:06 +0100

    jadwal

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-759.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCountdown.init( '.uagb-block-a59334a0', {"block_id":"a59334a0","endDateTime":"2024-04-07T18:30:00.000Z","showDays":true,"showHours":true,"showMinutes":true,"isFrontend":true,"timerEndAction":"zero","redirectURL":""} );
+});
+window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-6126be4a' );
+	UAGBTabs.anchorTabId( '.uagb-block-6126be4a' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-6126be4a' );
+}, false );
+ });
