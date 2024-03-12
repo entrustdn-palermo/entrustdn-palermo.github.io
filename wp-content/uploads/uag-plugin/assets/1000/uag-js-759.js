86b3e2c7567d359dead8ae8af5a15f0689515471
Arifin <arifin@Nurs-MacBook-Air.local>
2024-03-12 11:29:10 +0100

    attachment

--- wp-content/uploads/uag-plugin/assets/1000/uag-js-759.js
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-759.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
-	UAGBCountdown.init( '.uagb-block-a59334a0', {"block_id":"a59334a0","endDateTime":"2024-04-07T18:30:00.000Z","showDays":true,"showHours":true,"showMinutes":true,"isFrontend":true,"timerEndAction":"zero","redirectURL":""} );
+	UAGBCountdown.init( '.uagb-block-a59334a0', {"block_id":"a59334a0","endDateTime":"2024-04-08T18:30:00.000Z","showDays":true,"showHours":true,"showMinutes":true,"isFrontend":true,"timerEndAction":"zero","redirectURL":""} );
 });
 window.addEventListener( 'load', function() {
 	UAGBTabs.init( '.uagb-block-6126be4a' );
